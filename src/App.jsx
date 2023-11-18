1768e25512549e923ed40d8a1e56d5a56af5db47
mauriciomre <maurixmre@gmail.com>
2023-11-18 19:58:49 -0300

    build correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import "./css/bootstrap.min.css";
-import "./css/style.css";
-import "./js/bootstrap.min.js";
 
 function App() {
     return (
